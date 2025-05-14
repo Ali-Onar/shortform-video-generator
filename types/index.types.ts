62a6c4d06a2b6b08d8ec22e04dea53e55bea8c6a
Ali Onar <ali.onar@papak.io>
2025-05-14 15:21:33 +0300

    Add type definitions for InputData and OutputPaths interfaces in index.types.ts

---
+++ types/index.types.ts
@@ -0,0 +1,13 @@
+export interface InputData {
+    text: string;
+    musicPath: string;
+    videoPath: string;
+  }
+  
+  export interface OutputPaths {
+    audioPath: string;
+    subtitlePath: string;
+    finalVideo: string;
+    jsonPath: string;
+  }
+  
